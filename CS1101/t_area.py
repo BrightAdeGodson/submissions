29b04cb1111aac3a39a0f09091004f496a3bb19e
Mitsutaka Amano <MitsutakaAmano@uopeople.edu>
2018-07-21 10:01:42 +0900

    Add t_area.py for CS1101 Unit 2

---
+++ CS1101/t_area.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# assign the value of 12 to base and 16 to height.
+base = 12
+height = 16
+
+# calculate
+area = 0.5 * base * height
+
+# print the value of area
+print(area)
